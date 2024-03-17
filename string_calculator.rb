a13e86811b4dca361d691479c06d073681feef80
sheethaljadhav1998 <131395792+sheethaljadhav1998@users.noreply.github.com>
2024-03-17 19:54:09 +0530

    Update string_calculator.rb

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,6 +1,7 @@
 def str_calculator(str)
   #Split the string by commas
-  str = str.split(/,\s*/)
+  str = str.split(',')
+  return "empty string" if str.empty?
 
   # Convert strings to integers and sum them
   str.map(&:to_i).inject(0, :+)
