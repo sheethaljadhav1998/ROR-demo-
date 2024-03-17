3a620346e993c2e20bfde5933e93a8eb42bda1ae
sheethaljadhav1998 <131395792+sheethaljadhav1998@users.noreply.github.com>
2024-03-17 23:26:43 +0530

    Update string_calculator.rb

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,8 +1,37 @@
-def str_calculator(str)
-  # Checking empty string, Splitting the string by commas or newlines, Convert strings to integers and sum them
-   str.empty? ? "empty str" : str.split(/,\s*/).map(&:to_i).inject(0, :+)
+class StrCalculator
+  def initialize(str)
+    @str = str
+  end
+
+  def str_calculate
+    if @str.empty?
+      "empty str"
+    else
+      delimiter, numbers = extract_delimiter_and_numbers(@str)
+      numbers.split(delimiter).map(&:to_i).inject(:+)
+    end
+  end
+
+  private
+
+  def extract_delimiter_and_numbers(str)
+    if str.start_with?("//")
+      delimiter = str[2]
+      numbers = str[(str.index("\n") + 1)..-1]
+    else
+      delimiter = /[,\n]+/
+      numbers = str
+    end
+    [delimiter, numbers]
+  end
 end
 
-puts str_calculator("1\n2,3")   #Numbers separated by newline
+# Test cases
+calculator1 = StrCalculator.new("1\n2,3")
+puts calculator1.str_calculate  # Output: 6 (1 + 2 + 3)
 
-# Output: 6 (1 + 2 + 3)
+calculator2 = StrCalculator.new("//;\n1;2")
+puts calculator2.str_calculate  # Output: 3 (1 + 2)
+
+calculator3 = StrCalculator.new("3\n1")
+puts calculator3.str_calculate  # Output: 4 (3 + 1)
